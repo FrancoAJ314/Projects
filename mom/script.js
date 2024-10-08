377436949458f377f414ba8b1184f048733e714e
FrancoAJ314 <168702766+FrancoAJ314@users.noreply.github.com>
2024-10-07 20:21:48 -0700

    Add files via upload

--- mom/script.js
+++ mom/script.js
@@ -1,3 +1,30 @@
+// document.getElementById("card").addEventListener("click", flip)
+
+// function flip() {
+//     card = document.getElementById("card")
+//     // for (let i = 0; i < 100; i+=0.001) {
+//         //     card.transform = "rotateY("+i+"deg)"
+//         //     // card.style.transform = "rotateY(100deg)"
+//         //     console.log("rotateY("+i+"deg)");
+//         // }
+        
+        
+//     img = document.getElementById("img")
+//     if (img.alt != "bk"){
+//         card.style.transform = "rotateY(90deg)"
+//         img.src = "media/CardInside.webp"
+//         img.alt = "bk"
+//         card.style.transform = "rotateY(180deg)"
+//     }
+//     else {
+//         document.getElementById("img").src = "media/CardOutside.webp"
+//         img.alt = "ft"
+//         card.style.transform = "rotateY(0deg)"
+//     }
+    
+    
+// }
+
 (function() {
 
     var toggleBtn = document.querySelector('.toggle-btn'),
@@ -19,3 +46,4 @@
     
   
   })();
+
