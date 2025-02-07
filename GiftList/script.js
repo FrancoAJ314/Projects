4ca9f8f521cf89b582aa2e2d48ac0f4f39e3a965
FrancoAJ314 <168702766+FrancoAJ314@users.noreply.github.com>
2025-02-07 14:01:29 -0800

    Update script.js
    removed old code

--- GiftList/script.js
+++ GiftList/script.js
@@ -1,43 +1,24 @@
-// Sample JSON data
-
-filters = {
-  "modes":[],
-  "makes":[],
-  "models":[],
-  "designers":[]
-}
-
 activeCards = {}
 
 
 async function update_data() {
   fetch("data/list.json")
-    .then(response => response.json())
-    .then(dataDB => {
-      data = dataDB;
-      getCars();        
-    })
-    .catch(error => console.error("Error fetching data", error));
+  .then(response => response.json())
+  .then(dataDB => {
+    data = dataDB;
+    getCards();        
+  })
+  .catch(error => console.error("Error fetching data", error));
 }
 
-function getCars() {
-  cars = []
-
+function getCards() {
+  cards = []
+  
   data.forEach(item => {
-    if(filters["makes"].length==0 && filters["models"].length==0) {
-      cars.push(item)
-      activeCards[item.id] = item
-      // console.log("A");
-    }
-
-    // else if (and == [item.make, item.model, item.author]) { // ANDish behavior
-    else if (and.some(r=>[item.make, item.model, item.designer].includes(r))) { // OR behavior
-      cars.push(item)
-      // console.log(item.name, and.some(r=>[item.make, item.model, item.designer].includes(r)));
-    }
-
+    cards.push(item)
+    activeCards[item.id] = item
   });
-    generateCards(cars)
+  generateCards(cards)
 }
 
 
@@ -50,8 +31,8 @@ function createCard(item) {
   template.querySelector('.card-img').src = item.imageFront;
   template.querySelector('.card-name').textContent = item.name;
   template.querySelector('.url').href = item.purchase;
-
-  special = [10295]
+  
+  special = [1, 2, 10295]
   if (special.includes(item.id) ) {
     template.querySelector(".card").classList.add("special")
   }
@@ -65,10 +46,10 @@ function generateCards(db) {
   db.forEach(item => {
     // console.log(item);
     const card = createCard(item);
-
+    
     imgFt = card.querySelector(".card-img");
     imgBk = card.querySelector(".card-imgBk");
-
+    
     card.querySelectorAll(".img-container, .card-img, .card-img-bk, .card, .name, .view-btns").forEach(element => {
       element.addEventListener("mouseup", function(event) {
         if(event.target === element) {
@@ -78,8 +59,7 @@ function generateCards(db) {
           })
           this.closest(".card").style.border = "1px solid white";
           this.closest(".card").style.margin = "19px";
-          update_info_col(this.closest(".card"))
-      }
+        }
       })
     })
     
